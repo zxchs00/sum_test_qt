2a245d6aff06ee0f428abeb2500363e6d3b809ce
zxchs00 <zxchs00@gmail.com>
2016-07-09 00:59:26 -0700

    this is qtgit add sum_test_qt.pro

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "sum.h"
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    cout << sum(a) << endl;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,10 @@
+
+
+
+int sum(int n){
+	int i;
+	int res=0;
+	res = (n+1)*n/2;
+	return res;
+}
+
